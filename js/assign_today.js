18854d9d396ae24c5dde20feaa80fc86047c081f
Ian Fink <ian007@mac.com>
2025-04-08 06:03:06 -0500

    Initial commit

---
+++ js/assign_today.js
@@ -0,0 +1,26 @@
+/**
+ * @author		Ian M. Fink
+ *
+ * @file		assign_today.js
+ *
+ * @copyright	Copyright (C) Ian M. Fink.  All rights reserved.
+ *				This work is unpublished.
+ * 				A commercial license can be made available.
+ *
+ * Contact:		www.linkedin.com/in/ianfink
+ *
+ * Tabstop:		4
+ * 
+ */
+
+function
+assign_today(the_DOM_id, the_today)
+{
+	document.getElementById(the_DOM_id).value = the_today;
+	return false;
+} /* assign_today */
+
+/*
+ * End of file:	assign_today.js
+ */
+
