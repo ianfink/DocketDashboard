5784451b6a59d00871f5678245cdec667a3f5b26
Ian Fink <ian007@mac.com>
2025-04-08 06:16:27 -0500

    Initial commit

---
+++ body/default.php
@@ -0,0 +1,25 @@
+<?
+/**
+ * @author		Ian M. Fink
+ *
+ * @file		default.php
+ *
+ * @copyright	Copyright (C) 2025 Ian M. Fink.  All rights reserved.
+ * 				A commercial license can be made available.
+ *
+ * Contact:		www.linkedin.com/in/ianfink
+ *
+ * Tabstop:		4
+ */
+
+function
+dd_get_default_body_tag()
+{
+/*	return "<BODY BGCOLOR=\"#000000\" TEXT=\"#00FF00\">\n"; */
+	return "<BODY class=\"normal_body\">\n";
+} /* dd_get_body_tag */
+
+/*
+ * End of file: default.php
+ */
+?>
